d505499acaf6ce95d8a716039ac392e236402928
Dima <zaithev@gmail.com>
2015-07-30 00:07:12 +0300

    Initial ListFragments Project

---
+++ app/src/main/java/com/zaicev/activity/StartActivity.java
@@ -0,0 +1,75 @@
+package com.zaicev.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+
+import com.zaicev.R;
+import com.zaicev.fragments.ListFragment;
+
+/**
+ * Created by Dima on 29.07.2015.
+ */
+public class StartActivity extends FragmentActivity  {
+
+    static final String TAG = "myLogs";
+    static final int PAGE_COUNT = 3;
+
+    ViewPager pager;
+    PagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(pagerAdapter);
+
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected, position = " + position);
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public MyFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Title " + position;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return ListFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/zaicev/fragments/ListFragment.java
@@ -0,0 +1,53 @@
+package com.zaicev.fragments;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.zaicev.R;
+
+import java.util.Random;
+
+
+/**
+ * Created by Dima on 29.07.2015.
+ */
+public class ListFragment extends Fragment {
+    static final String ARGUMENT_PAGE_NUMBER = "arg_page_number";
+
+    int pageNumber;
+    int backColor;
+
+    public static ListFragment newInstance(int page) {
+        ListFragment pageFragment = new ListFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(ARGUMENT_PAGE_NUMBER, page);
+        pageFragment.setArguments(arguments);
+        return pageFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageNumber = getArguments().getInt(ARGUMENT_PAGE_NUMBER);
+
+        Random rnd = new Random();
+        backColor = Color.argb(140, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_fragment, null);
+
+        TextView tvPage = (TextView) view.findViewById(R.id.tvPage);
+        tvPage.setText("Page " + pageNumber);
+        tvPage.setBackgroundColor(backColor);
+
+        return view;
+    }
+}
